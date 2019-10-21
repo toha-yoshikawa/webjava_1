2954354caa3db0c6df0e8498083ebc8c65a7e144
systena <systena@DESKTOP-N55PLB9>
2019-10-21 19:19:41 +0900

    create new project

---
+++ src/main/java/jp/systena/tigerscave/springtest/application/controller/ItemListController.java
@@ -0,0 +1,24 @@
+package jp.systena.tigerscave.springtest.application.controller;
+
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@Controller // Viewあり。Viewを返却するアノテーション
+public class ItemListController {
+
+  @RequestMapping("/ItemList") // URLとのマッピング
+  public String index(HttpSession session, Model model,
+                            @RequestParam(name = "name", required = false) String name) {
+    // Viewに渡すデータを設定
+    model.addAttribute("name", name);
+    model.addAttribute("message", new java.util.Date());
+
+    // Viewのテンプレート名を返す
+    return "springhellosystena";
+  }
+}
